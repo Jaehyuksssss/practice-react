bef3103290fe78121591b251b3f712ebbf809dc4
Jaehyuksssss <ljh95310@naver.com>
2023-05-09 10:55:41 +0900

    ADD: Components

---
+++ practice/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Right from "./Selected";
+import Selected from "./Selected";
+
+const Home = () => {
+  return (
+    <div>
+      <Right />
+      <Selected />
+    </div>
+  );
+};
+
+export default Home;
