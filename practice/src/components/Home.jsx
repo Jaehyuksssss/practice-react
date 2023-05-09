4a7e2b168c97108cf478b6f919843088dfccef80
Jaehyuksssss <ljh95310@naver.com>
2023-05-09 14:25:38 +0900

    FIX : merged the function

--- practice/src/components/Home.jsx
+++ practice/src/components/Home.jsx
@@ -2,44 +2,41 @@ import React, { useEffect, useState } from "react";
 import Box from "./Box";
 
 const Home = () => {
-  const [leftBoxItems, setLeftBoxItems] = useState([]);
-  const [rightBoxItems, setRightBoxItmes] = useState([]);
-
+  const [boxItems, setBoxItems] = useState({ left: [], right: [] });
   const [selectedItem, setSelectedItem] = useState(null);
 
   useEffect(() => {
     fetch("/data/fetch.json", { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
-        setLeftBoxItems(data);
+        setBoxItems((prevItems) => ({ ...prevItems, left: data }));
       });
   }, []);
 
-  const handleLeftBoxItemClick = (item) => {
-    const newItems = leftBoxItems.filter((i) => i.id !== item.id);
-    setLeftBoxItems(newItems);
-    setRightBoxItmes([...rightBoxItems, item]);
-  };
-
-  const handleRightBoxItemClick = (item) => {
-    const newItems = rightBoxItems.filter((i) => i.id !== item.id);
-    setRightBoxItmes(newItems);
-    setLeftBoxItems([...leftBoxItems, item]);
+  const handleBoxItemClick = (item, boxSide) => {
+    console.log(boxItems[boxSide]);
+    const oppositeBoxSide = boxSide === "left" ? "right" : "left";
+    const newItems = boxItems[boxSide].filter((i) => i.id !== item.id);
+    setBoxItems((prevItems) => ({
+      ...prevItems,
+      [boxSide]: newItems,
+      [oppositeBoxSide]: [...prevItems[oppositeBoxSide], item],
+    }));
   };
 
   return (
     <div style={{ display: "flex", gap: "2rem" }}>
       <Box
-        items={leftBoxItems}
-        onItemClick={handleLeftBoxItemClick}
+        items={boxItems.left}
+        onItemClick={(item) => handleBoxItemClick(item, "left")}
         selectedItem={selectedItem}
-        onItemRemove={handleLeftBoxItemClick}
+        onItemRemove={(item) => handleBoxItemClick(item, "left")}
       />
       <Box
-        items={rightBoxItems}
-        onItemClick={handleRightBoxItemClick}
+        items={boxItems.right}
+        onItemClick={(item) => handleBoxItemClick(item, "right")}
         selectedItem={selectedItem}
-        onItemRemove={handleRightBoxItemClick}
+        onItemRemove={(item) => handleBoxItemClick(item, "right")}
       />
     </div>
   );
